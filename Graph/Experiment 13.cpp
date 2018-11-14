e19895ade6376de828f76e4b0089d32693fa272f
goel-amit <goelamit756@gmail.com>
2018-11-14 21:36:44 +0530

    commits

--- Graph/Experiment 13.cpp
+++ Graph/Experiment 13.cpp
@@ -1,51 +1,82 @@
-/*
-Program including all Operations on Graph and illustrate the traversals using DFS and BFS
-*/
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-class graph
+
+struct AdjListNode
 {
-  public:
-    int m, n, **arr;
-    graph(int a, int b)
-    {
-        m = a;
-        n = b;
-        arr = new int *[n];
-        for (int i = 0; i < m; i++)
-            arr[i] = new int[m];
-    }
-    void insert()
-    {
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (i == j)
-                    arr[i][j] = 0;
-                else
-                    cin >> arr[i][j];
-            }
-        }
-    }
-    void show()
-    {
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                cout << arr[i][j];
-            }
-            cout << endl;
-        }
-    }
+    int data;
+    AdjListNode *next;
 };
+
+struct AdjList
+{
+    AdjListNode *head;
+};
+
+struct Graph
+{
+    int V;
+    AdjList *arr;
+};
+
+AdjListNode *newAdjListNode(int);
+Graph *createGraph(int);
+void addEdge(Graph *, int, int);
+void printGraph(Graph *);
+
 int main()
 {
-    int m, n;
-    cin >> m >> n;
-    graph g(m, n);
-    g.insert();
-    g.show();
+    int totalVertices = 4;
+    Graph *graph;
+    graph = createGraph(totalVertices);
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 2);
+    addEdge(graph, 0, 3);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 2, 3);
+    printGraph(graph);
+}
+
+AdjListNode *newAdjListNode(int data)
+{
+    AdjListNode *nptr = new AdjListNode;
+    nptr->data = data;
+    nptr->next = NULL;
+    return nptr;
+}
+
+Graph *createGraph(int V)
+{
+    Graph *graph = new Graph;
+    graph->V = V;
+    graph->arr = new AdjList[V];
+    for (int i = 0; i < V; i++)
+    {
+        graph->arr[i].head = NULL;
+    }
+    return graph;
+}
+
+void addEdge(Graph *graph, int src, int dest)
+{
+    AdjListNode *nptr = newAdjListNode(dest);
+    nptr->next = graph->arr[src].head;
+    graph->arr[src].head = nptr;
+    nptr = newAdjListNode(src);
+    nptr->next = graph->arr[dest].head;
+    graph->arr[dest].head = nptr;
+}
+
+void printGraph(Graph *graph)
+{
+    for (int i = 0; i < graph->V; i++)
+    {
+        AdjListNode *root = graph->arr[i].head;
+        cout << "Adjacency list of vertex " << i << endl;
+        while (root != NULL)
+        {
+            cout << root->data << " -> ";
+            root = root->next;
+        }
+        cout << endl;
+    }
 }
